c12be2c1675549db0ea17a4690dc2b3f88ba6b32
Aryan Pandit <a12022005@gmail.com>
2024-04-05 23:31:47 +0530

    Darkmode

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,8 +1,14 @@
 <script lang="ts">
-  import { Button } from "$lib/components/ui/button";
+  import Lightswitch from "$lib/Lightswitch.svelte";
+import { Button } from "$lib/components/ui/button";
+  import { ModeWatcher } from "mode-watcher";
+  
 </script>
  
+
+<ModeWatcher />
 <main class="min-h-[100svh] w-screen bg-background flex justify-center items-center gap-5">
+  <Lightswitch />
   <Button href="/login">Login</Button>
   <Button href="/user">User</Button>
 </main>
