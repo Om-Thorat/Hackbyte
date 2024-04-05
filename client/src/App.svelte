9ebccf35b3e9d35d79c64173c95f14050bf13a17
Zapper9982 <emailtotanmay@gmail.com>
2024-04-06 01:41:04 +0530

    navbar

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
   import Isloggedin from "$lib/Isloggedin.svelte";
   import { ModeWatcher } from "mode-watcher";
+  import Navbar from "$lib/Navbar.svelte";
 </script>
+<Navbar />
 <ModeWatcher />
-<Isloggedin />
+<!-- <Isloggedin /> -->
+
 <main class="min-h-[100svh] w-screen bg-background  gap-5">
-  
+ 
 </main>
--- client/src/lib/Navbar.svelte
+++ client/src/lib/Navbar.svelte
@@ -2,9 +2,26 @@
     import { Button } from "$lib/components/ui/button";
     import Lightswitch from "$lib/Lightswitch.svelte";
 </script>
-<div>
-    <Lightswitch />
-    <Button href="/login">Login</Button>
-    <Button href="/user">User</Button>
-    <Button href="/logout">logout</Button>
+
+<style>
+ .navbar{
+    position:fixed;
+    top: 30px;
+    padding: 10px;
+    left :8%;
+    display: flex;
+    z-index: 1000;
+   
+ }
+
+</style>
+
+<div class="navbar flex justify-end bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 p-4 rounded-lg w-5/6 opacity-75">
+    <div class="flex space-x-4 ">
+        <Button href="/login">Login</Button>
+        <Button href="/user">User</Button>
+        <Button href="/logout">Logout</Button>
+        <Lightswitch />
+    </div>
 </div>
+   
