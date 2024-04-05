559424d1cca17a56bedb117c97df5c89625a778e
om thorat <thoratom1104@gmail.com>
2024-04-05 23:11:14 +0530

    isloggedin

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
+  import Isloggedin from "$lib/Isloggedin.svelte";
   import { Button } from "$lib/components/ui/button";
+  
 </script>
  
+<Isloggedin />
 <main class="min-h-[100svh] w-screen bg-background flex justify-center items-center gap-5">
   <Button href="/login">Login</Button>
   <Button href="/user">User</Button>
---
+++ client/src/lib/Isloggedin.svelte
@@ -0,0 +1,17 @@
+<script>
+    import { onMount } from 'svelte';
+
+    onMount(() => {
+        fetch('/user')
+            .then(response => response.text())
+            .then(data => {
+                if (data === 'No') {
+                    window.location.href = '/login';
+                }
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    });
+</script>
+
