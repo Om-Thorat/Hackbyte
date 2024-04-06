4b84fe0503516bf632c5e7e2f75e50c3a4448b47
Aryan Pandit <a12022005@gmail.com>
2024-04-06 07:31:31 +0530

    secondpage small changes

--- client/src/lib/Secondpage.svelte
+++ client/src/lib/Secondpage.svelte
@@ -1,16 +1,52 @@
 <script>
+    import Search from "./Search.svelte";
+
 
 </script>
 
 <div class="h-[100svh] w-[100svw] bg-background flex items-center justify-center ">
     <div class="bg-background h-[100svh] w-[20%]"><span>hellu</span></div>
-    <div class=" bg-slate-500 h-[95svh] w-[60%] rounded-sm m-1"><span>hi</span></div>
-    <div class="bg-background h-[100svh] w-[20%] flex flex-col justify-center items-center"><span>hellu</span>
-        <div class=" h-[40svh] w-[70%] m-3 ">
-            <h1>Some Leaderboard Title</h1>
+    <div class=" bg-gray-800 h-[95svh] w-[60%] rounded-sm m-1">
+        <Search />
+    </div>
+    <div class="bg-background h-[100svh] w-[20%] flex flex-col justify-center items-center">
+        <div class=" h-[40svh] w-[75%] m-3 border rounded-sm">
+            <h1 class="m-2">Some Leaderboard Title</h1>
+            <div class="flex">
+            <img src="./google-g-2015-logo-png-transparent.png" alt="" class="h-[3svh] w-[1.75svw] m-1">
+             <h2>stars</h2>
+            </div> 
+            <div class="flex">
+                <img src="./google-g-2015-logo-png-transparent.png" alt="" class="h-[3svh] w-[1.75svw] m-1">
+                <h2>stars</h2>
+            </div> 
+            <div class="flex">
+                <img src="./google-g-2015-logo-png-transparent.png" alt="" class="h-[3svh] w-[1.75svw] m-1">
+                <h2>stars</h2>
+            </div> 
+            <div class="flex">
+                <img src="./google-g-2015-logo-png-transparent.png" alt="" class="h-[3svh] w-[1.75svw] m-1">
+                <h2>stars</h2>
+            </div> 
         </div>
-        <div class=" h-[40svh] w-[70%] m-3 ">
-            <h1>Some Leaderboard Title</h1>
+        <div class=" h-[40svh] w-[75%] m-3 border rounded-sm">
+            <h1 class="m-2">Some Leaderboard Title</h1>
+            <div class="flex">
+                <img src="./google-g-2015-logo-png-transparent.png" alt="" class="h-[3svh] w-[1.75svw] m-1">
+                 <h2>stars</h2>
+                </div> 
+                <div class="flex">
+                    <img src="./google-g-2015-logo-png-transparent.png" alt="" class="h-[3svh] w-[1.75svw] m-1">
+                    <h2>stars</h2>
+                </div> 
+                <div class="flex">
+                    <img src="./google-g-2015-logo-png-transparent.png" alt="" class="h-[3svh] w-[1.75svw] m-1">
+                    <h2>stars</h2>
+                </div> 
+                <div class="flex">
+                    <img src="./google-g-2015-logo-png-transparent.png" alt="" class="h-[3svh] w-[1.75svw] m-1">
+                    <h2>stars</h2>
+                </div> 
         </div>
     </div>
 
