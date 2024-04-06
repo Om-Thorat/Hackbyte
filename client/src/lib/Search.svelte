2e72d9587abaa9dbf4ff2a5de73a3b4361d6eeda
om thorat <thoratom1104@gmail.com>
2024-04-06 09:35:50 +0530

    search better

--- client/src/lib/Search.svelte
+++ client/src/lib/Search.svelte
@@ -6,22 +6,26 @@
     async function getOrgs() {
         let r = await fetch("/AllOrganisations")
         Orgs = await r.json();
-        Sl = Orgs;
         console.log(Orgs);
     }
     getOrgs();
-    $: Sl = Orgs.filter((o) => o.organisation.toLowerCase().includes(inp.toLowerCase()));
+    function search(inp:string){
+      return Orgs.filter((o) => o.organisation.toLowerCase().includes(inp.toLowerCase()));
+    }
+    $: Sl = search(inp)
   </script>
    
   <Command.Root class="w-[45vw] border-2 border-white rounded-lg">
-    <Command.Input value={inp} placeholder="Type a command or search..." />
-    <Command.List>
-      <Command.Empty>No results found.</Command.Empty>
-      <Command.Separator />
-      <Command.Group heading="Companies">
-        {#each Sl as elem}
-            <Command.Item>{elem.organisation}</Command.Item>
-        {/each}
-      </Command.Group>
-    </Command.List>
+    <Command.Input bind:value={inp} placeholder="Type a command or search..." />
+      {#if inp !== ''}
+        <Command.List>
+          <Command.Empty>No results found.</Command.Empty>
+          <Command.Separator />
+          <Command.Group heading="Companies">
+            {#each Sl as elem}
+                <Command.Item>{elem.organisation}</Command.Item>
+            {/each}
+          </Command.Group>
+        </Command.List>
+      {/if}
   </Command.Root>
