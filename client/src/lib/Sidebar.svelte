7bc812ec4fa5a7ab28980dc2a1eec994acae4689
Zapper9982 <emailtotanmay@gmail.com>
2024-04-06 09:59:04 +0530

    sidebar

---
+++ client/src/lib/Sidebar.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import Button from "$lib/components/ui/button/button.svelte";
+  </script>
+
+
+<div class="bg-background h-screen w-[20%] flex flex-col py-8 justify-end bg-green-500 mt-4">
+    <div class="flex flex-col w-[100%] h-2/3 bg-orange-500 gap-12 align-center ">
+      <Button href="/home">Home</Button>
+      <Button href="/about">About</Button>
+      <Button href="/contact">Contact</Button>
+    </div>
+    <div class ="flex flex-col  gap-3  align-middle items-left justify-between  ">
+      <Button  href="/login">Login</Button>
+      <Button href="/user">User</Button>
+      <Button href="/logout">Logout</Button>
+    </div>
+  </div>
